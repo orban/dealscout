c7a1906a83b2447454640f1e82e7292217456b6c
aman-stelo <aman@stelolabs.com>
2024-03-02 22:49:21 -0800

    checkpoint

--- api/main.py
+++ api/main.py
@@ -1,13 +1,11 @@
-
-from fastapi import FastAPI, HTTPException, UploadFile, File
-from .image_ranker import ImageRanker
-from typing import List
-from .facebook_marketplace_scraper import FacebookMarketplaceScraper
+from fastapi import FastAPI, HTTPException, Request as APIRequest, Response, BackgroundTasks
+from api.image_ranker import ImageRanker
+from api.facebook_marketplace_scraper import FacebookMarketplaceScraper
 from typing import Dict, List
 from pydantic import BaseModel
-
 from api.db import create_new_request, get_latest_request, update_request_by_id
 from api.multion import MarketplaceAssistant  # noqa
+import api.whatsapp as whatsapp
 
 app = FastAPI()
 
@@ -21,6 +19,8 @@ async def rank_images_endpoint(reference_image_urls: List[str], candidate_images
         return results
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
 class ScrapeMarketplaceRequest(BaseModel):
     url: str
     reference_image_urls: List[str]
@@ -30,7 +30,7 @@ class ScrapeMarketplaceRequest(BaseModel):
 async def scrape_marketplace(request: ScrapeMarketplaceRequest):
     scraper = FacebookMarketplaceScraper(headless=False)
     items = await scraper.scrape(request.url)
-    try:   
+    try:
         # Initialize the ranker
         ranker = ImageRanker()
         results = ranker.rank_images(request.reference_image_urls, items)
@@ -39,51 +39,37 @@ async def scrape_marketplace(request: ScrapeMarketplaceRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.get("/")
-async def hello():
-    return {"message": "Hello World"}
-
-
-@app.post("/query")
-async def query(prompt: str):
-    """
-    Endpoint to handle queries to the MarketplaceAssistant.
-
-    :param prompt: The query string to be used for searching the marketplace.
-    :return: A JSON list of matching marketplace items.
-    """
-    agent = MarketplaceAssistant()
-    return await agent.filter(prompt)
-
-
-@app.post("/message_seller")
-async def message_seller(url: str):
-    """
-    Endpoint to handle messages to sellers on Facebook Marketplace.
-
-    :param urls: A list of URLs to be used for messaging the sellers.
-    :return: A JSON list of messages sent to the sellers.
-    """
-    agent = MarketplaceAssistant()
-    return await agent.message_seller(url)
-
-
-def get_text(body: Dict) -> str:
-    return "Hello World"
-
-
-def get_media(body: Dict) -> List[str]:
-    return ["https://example.com/image1.jpg", "https://example.com/image2.jpg"]
-
-
-def start_agent():
+def ranker(url: str, media: List[str]):
     pass
 
 
+def message_user(phone: str, urls: List[str]):
+    pass
+
+
+async def start_shopping(phone: str, prompt: str, media: List[str] = []):
+    agent = MarketplaceAssistant()
+    filter_res = await agent.filter(prompt)
+    search_url = filter_res['url']
+    product_urls = await ranker(search_url, media)
+    await agent.message_seller(product_urls)
+    message_user(phone, product_urls)
+
+
 @app.api_route("/sms_webhook", methods=["POST", "GET"])
-async def sms_webhook(body: Dict):
-    text = get_text(body)
-    media = get_media(body)
+async def sms_webhook(request: APIRequest, background_tasks: BackgroundTasks):
+    if request.method == "GET":
+        challenge = request.query_params.get("hub.challenge")
+        return Response(content=challenge, status_code=200, headers={"Content-Type": "text/plain"})
+
+    body = await request.json()
+    text = whatsapp.get_text(body)
+    media = whatsapp.get_media(body)
+    phone = whatsapp.get_phone(body)
+
+    print(f"Text: {text}")
+    print(f"Media: {media}")
+    print(f"Phone: {phone}")
 
     latest_request = get_latest_request()
 
@@ -102,11 +88,14 @@ async def sms_webhook(body: Dict):
             new_text = latest_request.text + "\n" + text
         if media is not None and len(media) > 0:
             updated_media_list = list(latest_request.media.values()) + media
-            updated_media = {
-                i: updated_media_list[i] for i in range(len(updated_media_list))
-            }
+            updated_media = {i: updated_media_list[i]
+                             for i in range(len(updated_media_list))}
+        else:
+            updated_media = latest_request.media
         update_request_by_id(latest_request.id, new_text,
                              updated_media, started)
-
-    if started:
-        start_agent()
+        if started:
+            print("Starting marketplace agent with prompt:", latest_request.text)
+            background_tasks.add_task(
+                start_shopping, phone, latest_request.text, media)
+        return "ok"
